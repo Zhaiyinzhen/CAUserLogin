dc2d328ea1e227466b8945411ed181e6a9201dee
Zhaiyinzhen <yinzhen.zhai@mail.utoronto.ca>
2024-10-22 21:38:48 -0400

    complete Phase 1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name the username
      */
     void setCurrentUser(String name);
+
+    /**
+     * Get current user.
+     * @return user name.
+     */
+    String getCurrentUser();
 }
